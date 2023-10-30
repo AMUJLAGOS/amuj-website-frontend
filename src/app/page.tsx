f34586120ef40e7e70b6679d24dcff603454677a
PeluMike <Pmichael3006@gmail.com>
2023-10-30 04:26:00 -0400

    more on responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
 import SocialImage from "@/components/SocialImage";
 import Footer from "@/components/Footer";
+import { GoArrowRight } from "react-icons/go";
 
 export default function Home() {
   const all = ["/collectionII_big.jpg", "/banner1.jpg"];
@@ -21,7 +22,7 @@ export default function Home() {
       <section className="absolute flex items-center w-full top-0">
         {/* banner 1  */}
         <div
-          className={`${style.banner} relative flex items-center justify-center`}
+          className={`${style.banner} relative items-center justify-center sm:flex hidden`}
         >
           <img src="/banner1.jpg" alt="collection i image" />
           <div
@@ -35,7 +36,7 @@ export default function Home() {
         </div>
         {/* banner 2  */}
         <div
-          className={`${style.banner} relative flex items-center justify-center`}
+          className={`${style.banner} relative sm:flex hidden items-center justify-center`}
         >
           <img src="/banner2.jpg" alt="collection ii image" />
           <div
@@ -47,11 +48,31 @@ export default function Home() {
             </Link>
           </div>
         </div>
+
+        {/* mobile  */}
+        <div className="sm:hidden block h-[600px]">
+          <img
+            src="/second_image.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <div className="flex justify-center">
+            <img
+              src="/amuj-logo.svg"
+              alt="amuj logo"
+              className="h-[50px] absolute bottom-9"
+            />
+          </div>
+        </div>
       </section>
       <Spacer height={640} />
-      <section>
-        <div className={`w-full relative ${style.collectionii}`}>
+      <section className="">
+        {/* destop  */}
+        <div
+          className={`w-full relative ${style.collectionii} sm:block hidden`}
+        >
           {/* <p>dd</p> */}
+
           <img
             src="/collectionII_big.jpg"
             alt="collection2"
@@ -62,6 +83,45 @@ export default function Home() {
               NEWEST CAMPAIGN
             </p>
             <h1 className="text-[30px] tracking-[5px]">COLLECTION II</h1>
+          </div>
+        </div>
+        {/* mobile  */}
+        <div className="sm:hidden flex flex-col justify-center items-center">
+          <div className="flex items-center">
+            <h1 className="mr-3">CAMPAIGNS</h1>
+            <GoArrowRight />
+          </div>
+          {/* collection  1  */}
+          <div className="relative w-full flex justify-center items-center mt-[20px]">
+            <img
+              src="/banner1.jpg"
+              alt="collection i image "
+              className="h-[380px] w-full object-cover"
+            />
+            <div className="absolute flex flex-col items-center">
+              <h1 className="text-[23px] text-white">COLLECTION I</h1>
+              <button className="bg-[#ffffffb4] text-[15px] py-1 px-4 mt-2">
+                SHOP NOW
+              </button>
+            </div>
+          </div>
+
+          {/* collection  2  */}
+          <div className="relative w-full flex justify-center items-center">
+            <img
+              src="/video.jpg"
+              alt="collection i image "
+              className="h-[380px] w-full object-cover"
+            />
+            <div className="absolute flex flex-col items-center">
+              <h1 className="text-[23px] text-white">COLLECTION II</h1>
+              <button className="bg-[#ffffffb4] text-[15px] py-1 px-4 mt-2">
+                SHOP NOW
+              </button>
+            </div>
+          </div>
+          <div>
+            <h1 className="tracking-[1px] py-4">COLLECTION II</h1>
           </div>
         </div>
       </section>
